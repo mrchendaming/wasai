153b4c7216195ce95888f99fc59abf043bae346f
runoob <810681612@qq.com>
2018-05-21 09:52:09 +0800

    message

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
 $(function(){
-	$(".container").hover(function(){
-		$(this).animate({opacity: 0.3}, 3000)
-	}, function(){
-		$(this).animate({opacity: 1}, 3000)
+	$("#banner").carousel({
+		imgs : [
+			{src:"imgs/login (1).jpg", href:"#"},
+			{src:"imgs/login (2).jpg", href:"#"},
+			{src:"imgs/login (3).jpg", href:"#"},
+			{src:"imgs/login (4).jpg", href:"#"},
+			{src:"imgs/login (5).jpg", href:"#"},
+			{src:"imgs/login (17).jpg", href:"#"}
+		],
+		width : 1920,
+		height : 525,
+		duration : 4000,
+		addPrevNextBtn: true
 	})
 })
